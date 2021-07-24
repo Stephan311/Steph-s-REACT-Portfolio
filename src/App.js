a1a160ad98193dc5596e83211994d5a36f12c5cb
stephan du Plooy <sdpphotography1@gmail.com>
2021-07-24 17:59:11 -0400

    styling changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 // TODO: Add a comment explaining what this function is doing
 // This function is a functional component that helps us split the UI into distinct parts.
@@ -9,7 +8,6 @@ function App() {
   return (
     <div>
    <Header />
-   <Footer />
    </div>
 
   )
